0908c0e254e4ae0609a3c96e07f5e697b4aced8b
erac2 <71734077+erac2@users.noreply.github.com>
2020-09-29 19:39:05 -0400

    Update average.py

--- average.py
+++ average.py
@@ -1,11 +1,17 @@
-numberOfScores= 0
-score1= 0
-score2= 0
-total= 0
-average= 0.0
-scoreCount= 0
-#numberOfScores = input("Please enter the number of scores you want to average: ")
-score1= int(input("Please enter a test score: "))
-score2= int(input("Please enter a test score: "))
-average = (score1+score2)/ 2
-print(average)
+numberOfScores = 0
+score1 = 0
+score2 = 0
+total = 0
+average = 0.0
+scoreCount = 0
+numberOfScores = int(input("Please enter the number of scores you want to average: "))
+
+#What python structure could we use to repeat the next 3 lines
+while scoreCount < numberOfScores:
+    score = int(input("Please enter a score: "))
+    total = total + score
+    scoreCount = scoreCount + 1
+
+if scoreCount == numberOfScores :
+    average = (total) / numberOfScores
+    print(average)
